f5ea64754b720554bd8520d443d83bbf761b743d
AQ <cshazemelwazery@gmail.com>
2024-06-19 16:23:32 +0300

    add slashapp in main

---
+++ lib/slash_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SlashApp extends StatelessWidget{
+  const SlashApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+    );
+  }
+
+
+
+}
