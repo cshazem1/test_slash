49ff4adc10ace9f8a325596ee42b33955fccc5b4
AQ <cshazemelwazery@gmail.com>
2024-06-19 18:19:13 +0300

    add theming

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,5 +1,5 @@
 class Routes{
-  static String home="/";
+  static const String home="/";
 
 
 }
---
+++ lib/core/theming/styles.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+
+import '../../constans.dart';
+
+abstract class TextStyles{
+static TextStyle  font20Black700Weight=const TextStyle(
+  fontWeight: FontWeight.w700,
+  fontSize: 20,fontFamily: kPrimaryFont
+
+);
+static TextStyle  font14Black700Weight=const TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 14,fontFamily: kPrimaryFont
+
+);
+static TextStyle  font14Black400Weight=const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 14,fontFamily: kPrimaryFont
+
+);
+static TextStyle  font22Black600Weight=const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 22,fontFamily: kPrimaryFont
+
+);
+static TextStyle  font14Black500Weight=const TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,fontFamily: kPrimaryFont
+
+);
+static TextStyle  font16Black700Weight=const TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 16,fontFamily: kPrimaryFont
+
+);
+}
